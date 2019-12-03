f6af36ad4c64993ccf271ecb656015a445e1c3cf
Katherine Gerot <katie.gerot@huskers.unl.edu>
2019-12-03 10:46:05 -0600

    advent 3

--- 2019/03.py
+++ 2019/03.py
@@ -1,27 +1,11 @@
-import sys
-
-f = open(sys.argv[1], "r")
-content = f.read()
-lines = content.split(",")
+lines = list(map(int, open("intcode.txt", "r").read().split(",")))
 i = 0
-while i < len(lines) :
-    code = lines[i]
-    if code == 1 :
-        a = lines[i + 1]
-        b = lines[i + 2]
-        out = lines[i + 3]
-        lines[out] = lines[a] + lines[b]
-        i+=3
-    elif code == 2 :
-        a = lines[i + 1]
-        b = lines[i + 2]
-        out = lines[i + 3]
-        lines[out] = lines[a] * lines[b]
-        i+=3
-    elif code == 99 :
-        print("HALT: 99,", i)
-        break;
-    else :
-        print("Something went wrong, CODE:", code, "INDEX:", i)
-    i+=1
+while i < len(lines):
+    if lines[i] == 1:
+        lines[lines[i + 3]] = lines[lines[i + 1]] + lines[lines[i + 2]]
+    elif lines[i] == 2:
+        lines[lines[i + 3]] = lines[lines[i + 1]] * lines[lines[i + 2]]
+    else:
+        break
+    i+= 4 if lines[i] == 1 or lines[i] == 2 else 1
 print(lines)
